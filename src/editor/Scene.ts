72fa84a39cfee9a1a7929aa73716b833d51f1c19
ljr <1368298676@qq.com>
2023-10-23 16:58:54 +0800

    feat: scene render节流

--- src/editor/Scene.ts
+++ src/editor/Scene.ts
@@ -1,7 +1,7 @@
 import Editor from './editor';
 import EventEmitter from './eventEmitter';
 import Shape from './shape/Shape';
-import { getDevicePixelRatio } from './utils';
+import { getDevicePixelRatio, rafThrottle } from './utils';
 interface Events {
   render(): void;
 }
@@ -17,40 +17,56 @@ export default class Scene {
       this.shapes.push(shape);
     }
   }
-  render() {
-    const { editor } = this;
-    const {
-      canvasContext: ctx,
-      setting,
-      canvasElement: canvas,
-      viewportManager,
-      zoomManager,
-    } = editor;
-    const viewport = viewportManager.getViewport();
+  render = rafThrottle(() => {
+    {
+      const { editor } = this;
+      const {
+        canvasContext: ctx,
+        setting,
+        canvasElement: canvas,
+        viewportManager,
+        zoomManager,
+      } = editor;
+      const viewport = viewportManager.getViewport();
 
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    // 2. 清空画布，然后绘制所有可见元素
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // 绘制背景色
-    ctx.save();
-    ctx.fillStyle = setting.get('canvasBgColor');
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();
-
-    // 场景坐标转换为视口坐标
-    const dpr = getDevicePixelRatio();
-    const zoom = zoomManager.getZoom();
-    const dx = -viewport.x;
-    const dy = -viewport.y;
-    ctx.scale(dpr * zoom, dpr * zoom);
-    ctx.translate(dx, dy);
-
-    this.shapes.forEach((shape) => {
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      // 2. 清空画布，然后绘制所有可见元素
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      // 绘制背景色
       ctx.save();
-      shape.draw(ctx);
+      ctx.fillStyle = setting.get('canvasBgColor');
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.restore();
-    });
 
-    this.eventEmitter.emit('render');
+      // 场景坐标转换为视口坐标
+      const dpr = getDevicePixelRatio();
+      const zoom = zoomManager.getZoom();
+      const dx = -viewport.x;
+      const dy = -viewport.y;
+      ctx.scale(dpr * zoom, dpr * zoom);
+      ctx.translate(dx, dy);
+
+      this.shapes.forEach((shape) => {
+        ctx.save();
+        shape.draw(ctx);
+        ctx.restore();
+      });
+
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.save();
+      if (setting.get('enableRuler')) {
+        this.editor.ruler.draw();
+      }
+      ctx.restore();
+
+      this.eventEmitter.emit('render');
+    }
+  });
+
+  on(eventName: 'render', handler: () => void) {
+    this.eventEmitter.on(eventName, handler);
+  }
+  off(eventName: 'render', handler: () => void) {
+    this.eventEmitter.off(eventName, handler);
   }
 }
